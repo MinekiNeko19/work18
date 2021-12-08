ac5ef27c58ed070b705055b0c12d2ecbd0afec43
Sarah Leong <sleong20@stuy.edu>
2021-12-08 15:51:22 -0500

    passes information b/w processes correctly

--- pipe.c
+++ pipe.c
@@ -8,14 +8,20 @@
 #define WRITE 1
 
 int main() {
+    int ptoc[2];
     int ctop[2];
+    char line[100];
     pipe(ctop);
+    pipe(ptoc);
     int f = fork();
 
     if (f) { // parent
         // closing pipes
         close(ctop[WRITE]);
+        close(ptoc[READ]);
         // gets input
+        fgets(line, 100, stdin);
+        write(ptoc[WRITE], line, 100);
         // sends to child
         // reads from child            
         char line[100];
@@ -25,10 +31,12 @@ int main() {
     else { // child
         // closing pipes
         close(ctop[READ]);
+        close(ptoc[WRITE]);
         // read from parent
+        read(ptoc[READ], line, 100);
         // random
         // send to parent
-        write(ctop[WRITE], "hello", 6);
+        write(ctop[WRITE], line, 100);
     }
     return 0;
 }
