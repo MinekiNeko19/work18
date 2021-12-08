5eb3c9b3d6753b8695dba2c30989bcc9c4c9e398
Sarah Leong <sleong20@stuy.edu>
2021-12-08 15:35:59 -0500

    the problem was fork should be after pipe aldkfjalskdfjasldkjfakls

--- pipe.c
+++ pipe.c
@@ -8,42 +8,27 @@
 #define WRITE 1
 
 int main() {
-    int ptoc[2];
     int ctop[2];
-    char line[100];
+    pipe(ctop);
     int f = fork();
 
-    pipe(ptoc);
-    pipe(ctop);
-        
-    while (1) {
-        if (f) { // parent
-            // closing pipes
-            close(ptoc[READ]);
-            close(ctop[WRITE]);
-
-            // gets input
-            printf("Enter input (type exit to end program): ");
-            fgets(line, 100, stdin);
-            if (strcmp(line,"exit")) {
-                exit(0);
-            }
-            write(ptoc[WRITE], line, 100);
-        }
-        else { // child
-            // closing pipes
-            close(ptoc[WRITE]);
-            close(ctop[READ]);
-
-            // read from parent
-            read(ptoc[READ], line, 100);
-            // printf("from parent: %s\n",line);
-            line[0] = 'p';
-            write(ctop[WRITE], line, 100);
-
-            // random
-            // printf("Child: i can't read so that sign won't stop me\n");
-        }
+    if (f) { // parent
+        // closing pipes
+        close(ctop[WRITE]);
+        // gets input
+        // sends to child
+        // reads from child            
+        char line[100];
+        read(ctop[READ], line, 100);
+        printf("parent got: %s\n",line);
+    }
+    else { // child
+        // closing pipes
+        close(ctop[READ]);
+        // read from parent
+        // random
+        // send to parent
+        write(ctop[WRITE], "hello", 6);
     }
     return 0;
 }
