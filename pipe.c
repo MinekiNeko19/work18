1e5e9b2a25e66aaae64e6ff2a4572d38aa5877ee
Sarah Leong <sleong20@stuy.edu>
2021-12-08 11:22:59 -0500

    forking and getting input

--- pipe.c
+++ pipe.c
@@ -2,6 +2,24 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define READ 0
+#define WRITE 1
+
 int main() {
+    // gets input
+    char line[100];
+    printf("Enter input: ");
+    fgets(line, 100, stdin);
+    printf("%s\n",line);
+
+
+    int f = fork();
+    if (f) { // parent
+        printf("Parent: %s\n", line);
+    }
+    else { // child
+        printf("Child: i can't read so that sign won't stop me\n");
+    }
+
     return 0;
 }
